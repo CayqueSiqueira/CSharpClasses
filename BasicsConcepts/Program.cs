c7bee7520c7c5794f2a606dbbe23e9fd7c7d7595
202210966 <202210966@TPCP04LAB1211>
2024-03-06 19:29:18 -0300

    Adicionar arquivos de projeto.

---
+++ BasicsConcepts/Program.cs
@@ -0,0 +1,62 @@
+﻿bool prossiga = true;
+
+do
+
+{
+
+    try //tratativa de erro 
+
+    {
+
+        Console.WriteLine("Informe o peso");
+        float peso = float.Parse(Console.ReadLine());//Console.ReadLine() entrada de dado 
+
+        Console.WriteLine("Informe altura");
+        float altura = float.Parse(Console.ReadLine());
+
+        float imc = peso / (altura * altura);
+        Console.WriteLine($"o imc é {imc:f1}"); //o :f1 é pra tratar as casas depois da virgula 
+        if (imc < 18.5)
+        {
+            Console.WriteLine("Abaixo do peso");
+            break;
+        }
+        else if (imc < 24.9)
+        {
+            Console.WriteLine("Peso normal");
+            break;
+        }
+        else if (imc < 29.9)
+        {
+            Console.WriteLine("Sobrepeso");
+            break;
+        }
+        else if (imc < 34.9)
+        {
+            Console.WriteLine("Obesidade grau 1 ");
+            break;
+        }
+        else if (imc < 39.9)
+        {
+            Console.WriteLine("Obesidade grau 2");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Obesidade grau 3");
+        }
+        prossiga = false;
+
+    }
+
+    catch (FormatException e)//variavel q vai ser o erro 
+
+    {
+
+        Console.WriteLine("Informe apenas números");
+
+        Console.WriteLine(e.Message);// exime a mensagem que ferou o erro 
+
+    }
+
+} while (prossiga);
